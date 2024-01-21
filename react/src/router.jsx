f683d3c9a296e465e7b204922f5c5d0e41e03adf
Saputra <saputra.uta50@gmail.com>
2024-01-21 14:22:52 +0800

    context prodiver, protected routes

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,25 +1,48 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Users from "./pages/Users";
 import NotFound from "./pages/NotFound";
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
+import Dashboard from "./pages/Dashboard";
 
 const router = createBrowserRouter([
     {
-        path: "/login",
-        element: <Login/>,
+        path: "/",
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Navigate to="/users"/>
+            },
+            {
+                path: "/dashboard",
+                element: <Dashboard />,
+            },
+            {
+                path: "/users",
+                element: <Users />,
+            },
+        ],
     },
     {
-        path: "/signup",
-        element: <SignUp/>,
-    },
-    {
-        path: "/users",
-        element: <Users/>,
+        path: "/",
+        element: <GuestLayout />,
+        children: [
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/signup",
+                element: <SignUp />,
+            },
+        ],
     },
     {
         path: "*",
-        element: <NotFound/>,
+        element: <NotFound />,
     },
 ]);
 
